83477db81b82d26d4eddf3c4c45543e31adc9c94
kuldip-trentium <kuldip@trentiums.com>
2023-08-16 15:43:46 +0530

    update : policy text

--- src/atoms/policy/Policy.js
+++ src/atoms/policy/Policy.js
@@ -4,7 +4,7 @@ import './policy.css'
 const Policy = () => {
   return (
     <div className='login-form-footer'>
-      <span className='login-form-footer-upper'>By using the app | agree to the sport</span> <br />
+      <span className='login-form-footer-upper'>By using the app I agree to the sport</span> <br />
       <span className='login-form-footer-lower'>Terms of Service & privacy Policy</span>
     </div>
   )
