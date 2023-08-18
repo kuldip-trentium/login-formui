d052ea0e33953dc7b91db7faa96cf2d300400db7
kuldip-trentium <kuldip@trentiums.com>
2023-08-18 14:34:33 +0530

    update : remove fragment from Form

--- src/molecules/form/Form.js
+++ src/molecules/form/Form.js
@@ -10,29 +10,27 @@ const FormFields = () => {
     console.log(values);
   };
   return (
-    <>
-      <div className='custom-inputs'>
-        <Formik
-          initialValues={{
-            name: '',
-            email: '',
-          }}
-          validationSchema={Yup.object({
-            name: Yup.string().required('Name is required'),
-            email: Yup.string().email('Invalid email address').required('Email is required'),
-          })}
-          onSubmit={handleSubmit}
-        >
-          <Form>
-            <Input type='text' placeholder='Name' name='name' image={user} />
-            <Input type='email' placeholder='Email' name='email' image={user} />
-            <Button type='submit' text='Continue'>Continue</Button>
-          </Form>
-        </Formik>
+    <div className='custom-inputs'>
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string().required('Name is required'),
+          email: Yup.string().email('Invalid email address').required('Email is required'),
+        })}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <Input type='text' placeholder='Name' name='name' image={user} />
+          <Input type='email' placeholder='Email' name='email' image={user} />
+          <Button type='submit' text='Continue'>Continue</Button>
+        </Form>
+      </Formik>
 
-      </div>
+    </div>
 
-    </>
   )
 }
 
